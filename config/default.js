5d4c27af87a80f46e9fa26bcbbaa20c321103aee
Heaven <1302888286@qq.com>
2018-04-24 00:19:06 +0800

    Update

--- config/default.js
+++ config/default.js
@@ -5,6 +5,6 @@ module.exports = {
     key: 'myblog',
     maxAge: 2592000000
   },
-  mongodb: 'mongodb://localhost:27017/myblog'
+  mongodb: 'mongodb://39.108.168.223:27017/admin'
   //mongodb 的地址，以 mongodb:// 协议开头，myblog 为 db 名
 }
